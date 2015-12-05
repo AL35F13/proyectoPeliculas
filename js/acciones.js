181e8ac29a42393e01fe65275652d50422d01f50
AL35F13 <anahi2028@gmail.com>
2015-12-05 10:35:50 -0600

    tres
    jaja

--- js/acciones.js
+++ js/acciones.js
@@ -5,7 +5,7 @@ document.addEventListener("deviceready",function(){
 		alert("dentro");
 		  $.ajax({
 			  type: "POST",
-			  url: "http://192.168.1.165/proyecto/buscartodas.php",
+			  url: "http://192.168.1.163/proyecto/buscartodas.php",
 			  error: function(){
 				  alert("No se puede conectar al servidor")
 				  },
