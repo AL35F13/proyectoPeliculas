ca8980b0ec9c07b879b8bab02e092632996b81f2
AL35F13 <anahi2028@gmail.com>
2015-12-07 12:26:54 -0600

    10

--- js/acciones.js
+++ js/acciones.js
@@ -2,7 +2,16 @@
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	$("#btnTodo").on('tap', function(){
-		alert ("here");
+		 $.ajax({
+			  type: "POST",
+			  url: "http://192.168.1.163/proyecto/buscartodas.php",
+			  data: "",
+			  error: function(){
+				  alert("No se puede conectar al servidor");},
+		      success: function(respuesta){
+				  alert (respuesta);
+				}
+		  });
 		 });
 					 
 		  
