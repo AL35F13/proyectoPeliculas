0e4681c4f89a3d81cb84069c3ea414df91e5b2eb
AL35F13 <anahi2028@gmail.com>
2015-12-07 13:29:16 -0600

    17

--- js/acciones.js
+++ js/acciones.js
@@ -11,10 +11,21 @@ document.addEventListener("deviceready",function(){
 				  },
 		      success: function(respuesta)
 			  {
-				   alert("a->"+respuesta+"<-");
-				  var producto =JSON.parse(respuesta);
 				  alert(respuesta);
-				  alert(producto.peliculas[0].nombrePelicula);
+				  var producto =JSON.parse(respuesta);
+				  
+			   $('#contenido').empty();
+			   
+			   $(':mobile-pagecontainer').pagecontainer('change','#todas',{
+				transition:'pop'
+				});
+			   
+			   for(var $x=0; $x<producto.peliculas.length;$x++)
+			   
+			   {
+				   
+				   $('#contenido').append('<div><div>Nombre de la Película:'+ producto.peliculas[$x].nombrePelicula +'</div><div><div style="float:left; width:30%;"><img src="http://192.168.1.163/proyecto/recursos/imagenes/fotos/'+ producto.peliculas[$x].clavePelicula +'.jpg"></div><div style="float:left; width:70%;"><div><div style="float:left; width:50%;">Clasificación:'+ producto.peliculas[$x].clasificacion +'</div><div style="float:left; width:50%;">Género:'+ producto.peliculas[$x].genero +'</div></div><div style="clear:both;"><div style="float:left; width:50%;">Idioma:'+ producto.peliculas[$x].idioma +'</div><div style="float:left; width:50%;">Duración:'+ producto.peliculas[$x].duracion +'</div></div><div style="clear:both;">Director:'+ producto.peliculas[$x].director +'</div></div></div><div style="clear:both;">Sinópsis:'+ producto.peliculas[$x].sinopsis +'</div></div>');
+				   }
 			   
 				}
 		 });
