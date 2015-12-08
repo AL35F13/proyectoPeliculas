18aa16e4b26ca48d86ee5e73366be9e6fe005f61
AL35F13 <anahi2028@gmail.com>
2015-12-07 23:29:19 -0600

    21

--- js/acciones.js
+++ js/acciones.js
@@ -33,7 +33,7 @@ document.addEventListener("deviceready",function(){
 		  $("#btnbuscar").on('tap', function(){
 		 $.ajax({
 			  type: "POST",
-			  url: "http://192.168.1.127/proyecto/buscartodas.php",
+			  url: "http://192.168.1.127/proyecto/BusquedaNombre.php",
 			  data: "nombrePelicula=" + $('#txtnombrePelicula').val(),
 			  error: function(){
 				  alert("No se puede conectar al servidor");
@@ -50,7 +50,7 @@ document.addEventListener("deviceready",function(){
 			   
 			   {
 				   
-				   $('#contenidoNombre').append('<div><div>Nombre de la Película:'+ producto.peliculas[$x].nombrePelicula +'</div><div><div style="float:left; width:30%;"><img src="http://192.168.1.127/proyecto/recursos/imagenes/fotos/'+ producto.peliculas[$x].clavePelicula +'.jpg"></div><div style="float:left; width:70%;"><div><div style="float:left; width:50%;">Clasificación:'+ producto.peliculas[$x].clasificacion +'</div><div style="float:left; width:50%;">Género:'+ producto.peliculas[$x].genero +'</div></div><div style="clear:both;"><div style="float:left; width:50%;">Idioma:'+ producto.peliculas[$x].idioma +'</div><div style="float:left; width:50%;">Duración:'+ producto.peliculas[$x].duracion +'</div></div><div style="clear:both;">Director:'+ producto.peliculas[$x].director +'</div></div></div><div style="clear:both;">Sinópsis:'+ producto.peliculas[$x].sinopsis +'</div></div>');
+				   $('#contenidoNombre').append('<div><div>Nombre de la Película:'+ producto.peliculas[$x].nombrePelicula +'</div><div><div style="float:left; width:30%;"><img src="http://192.168.1.127/proyecto/recursos/imagenes/fotos/'+ producto.peliculas[$x].clavePelicula +'.jpg" style="height:20px;width:20px;"></div><div style="float:left; width:70%;"><div><div style="float:left; width:50%;">Clasificación:'+ producto.peliculas[$x].clasificacion +'</div><div style="float:left; width:50%;">Género:'+ producto.peliculas[$x].genero +'</div></div><div style="clear:both;"><div style="float:left; width:50%;">Idioma:'+ producto.peliculas[$x].idioma +'</div><div style="float:left; width:50%;">Duración:'+ producto.peliculas[$x].duracion +'</div></div><div style="clear:both;">Director:'+ producto.peliculas[$x].director +'</div></div></div><div style="clear:both;">Sinópsis:'+ producto.peliculas[$x].sinopsis +'</div></div>');
 				   }
 			   $(':mobile-pagecontainer').pagecontainer('change','#ResultadoNombre',{
 				transition:'pop'
